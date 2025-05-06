8b8e99d7fbc2bfc62d849c99eafe69362cdf6129
Emmanuel Etti (eetti.eth) <etti.emman@gmail.com>
2025-05-06 17:23:55 -0300

    Update run-simulation.sh

--- run-simulation.sh
+++ run-simulation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-# echo "B00937817" | sudo -S python3 /users/grad/etti/pinet/stream2gym/main.py /users/grad/etti/pinet/stream2gym/use-cases/disconnection/military-coordination/input.graphml --time 100 &
+# echo "pass" | sudo -S python3 /users/grad/etti/pinet/stream2gym/main.py /users/grad/etti/pinet/stream2gym/use-cases/disconnection/military-coordination/input.graphml --time 100 &
 # main_pid=$!
 
 echo "???" | sudo -S python3 run_experiments.py
