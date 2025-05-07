e308a54648a4644f28232ce57fae21633bd647f2
Emmanuel Etti <etti.emman@gmail.com>
2025-05-07 13:27:39 -0300

    updated cleanup script

--- cleanup.py
+++ cleanup.py
@@ -4,3 +4,5 @@ os.system("sudo mn -c ")
 os.system("sudo ps aux | grep 'bandwidth-monitor' | grep python | awk '{print $2}' | sudo xargs kill -9")
 os.system("sudo ps aux | grep 'run_experiments' | grep python | awk '{print $2}' | sudo xargs kill -9")
 os.system("sudo ps aux | grep '/users/grad/etti/pinet' | grep python | awk '{print $2}' | sudo xargs kill -9")
+os.system("sudo ps aux | grep 'use-cases/disconnection' | grep python | awk '{print $2}' | sudo xargs kill -9")
+
